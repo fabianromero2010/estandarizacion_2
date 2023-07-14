9ea08d02a3f54107648602dad6f4a2008407f688
Alejandra-byte-pixel <138229513+Alejandra-byte-pixel@users.noreply.github.com>
2023-07-14 09:33:37 -0500

    main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ if nombre_archivo is not None:
     )
 
 else:
-    st.warning("Por favor, selecciona un archivo para cargar.")
-
+    st.warning("<span style='color: white;'>Por favor, selecciona un archivo para cargar.</span>", unsafe_allow_html=True)
+    
 # Cerrar el contenedor principal
 st.markdown("</div>", unsafe_allow_html=True)
