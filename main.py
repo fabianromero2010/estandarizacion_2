1a5b3de7d491abe01d6df948623161f867c6c33e
Alejandra-byte-pixel <138229513+Alejandra-byte-pixel@users.noreply.github.com>
2023-07-14 00:40:28 -0500

    main.py

--- main.py
+++ main.py
@@ -29,8 +29,7 @@ st.markdown("<div class='main'>", unsafe_allow_html=True)
 # Mostrar el título y la imagen en la parte principal
 st.markdown("<h1 style='color: white; text-align: center;'>Aplicación para la Estandarización de teléfonos nacionales</h1>", unsafe_allow_html=True)
 image = Image.open("LogoSETI.jpeg")
-st.image(image, caption='Logo', use_column_width=True)
-
+st.image(image, caption='SETI',  width=200)
 Dataframe1 = {
     "cadenas": [],
     "tipo": [],
