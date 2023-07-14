e6f6efe52fc3e14e24bc9a60c9f1d62ded435900
Alejandra-byte-pixel <138229513+Alejandra-byte-pixel@users.noreply.github.com>
2023-07-14 10:21:13 -0500

    main.py

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ st.markdown(
 # Barra lateral (sidebar)
 st.sidebar.image("LogoAIO.jpeg", caption='ALL IN ONE', use_column_width=True)
 st.sidebar.markdown("<h1 style='text-align: center; color: black;'>Aquí cargue el archivo</h1>", unsafe_allow_html=True)
-#nombre_archivo = st.sidebar.file_uploader("Cargue archivo TXT", type=["txt"], key="file-upload")
+nombre_archivo = st.sidebar.file_uploader("Cargue archivo TXT", type=["txt"], key="file-upload", help='Limite 200MB')
 
-nombre_archivo = st.file_uploader(label='Examinar archivo TXT', type=["txt"], accept_multiple_files=False, help='Limite 200MB')
+#nombre_archivo = st.file_uploader(label='Examinar archivo TXT', type=["txt"], accept_multiple_files=False, help='Limite 200MB')
     
 
 # Contenedor principal
