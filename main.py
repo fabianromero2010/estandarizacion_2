95b02ed22f9b8ebfa5663b173f67b1ff9e142b9f
Alejandra-byte-pixel <138229513+Alejandra-byte-pixel@users.noreply.github.com>
2023-07-14 01:44:33 -0500

    main.py

--- main.py
+++ main.py
@@ -65,11 +65,19 @@ if nombre_archivo is not None:
     df = pd.DataFrame(Dataframe1)
 
     # Mostrar el resultado en una tabla en la columna derecha
-    col2.title("Resultado:")
+    col2.markdown("""
+        <div style="position: sticky; top: 0;">
+            <h2>Resultado:</h2>
+        </div>
+    """, unsafe_allow_html=True)
     col2.dataframe(df)
 
     # Crear botón para descargar archivo csv en la columna derecha
-    col2.download_button('Download CSV', datos, file_name=name + '.csv')
+    col2.markdown("""
+        <div style="position: sticky; top: 50px;">
+            <a href="data:file/csv;base64,{b64}" download="{file_name}.csv">Download CSV</a>
+        </div>
+    """.format(b64=datos.encode().hex(), file_name=name), unsafe_allow_html=True)
 
 else:
     st.warning("Por favor, selecciona un archivo para cargar.")
