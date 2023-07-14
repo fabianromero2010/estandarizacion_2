99cf3ac86585b3ef26f2bdef51b9531fc81b7b4a
Alejandra-byte-pixel <138229513+Alejandra-byte-pixel@users.noreply.github.com>
2023-07-14 01:10:03 -0500

    main.py

--- main.py
+++ main.py
@@ -30,21 +30,21 @@ nombre_archivo = st.sidebar.file_uploader("Selecciona un archivo", type=["txt"])
 # Contenedor principal
 st.markdown("<div class='main'>", unsafe_allow_html=True)
 
-# Mostrar el título y la imagen en la parte principal
-st.markdown("<h1 style='color: white;'>Aplicación para la Estandarización de teléfonos nacionales</h1>", unsafe_allow_html=True)
+# Dividir la parte derecha en columnas
+    col1, col2 = st.beta_columns(2)
 
-# Imagen a la derecha en la parte principal
-image = Image.open("LogoSETI.jpeg")
-st.image(image, caption='SETI', width=50)
+separador_campos = ';'
+st.title("Aplicación Estandarización de telefonos nacionales")
 
+nombre_archivo = st.file_uploader("Selecciona un archivo", type=["txt"])
 
-Dataframe1 = {
-    "cadenas": [],
-    "tipo": [],
+Dataframe1 = {"cadenas": [],
+    "tipo":[],
     "indicativos_pais": [],
     "indicativos_area": [],
-    "telefonos": []
+    "telefonos":[]
 }
+
 
 if nombre_archivo is not None:
     contenido = nombre_archivo.read().decode("utf-8")  # Leer el contenido del archivo
@@ -52,33 +52,32 @@ if nombre_archivo is not None:
 
     datos = "CADENA;TIPO;INDICATIVO_PAIS;INDICATIVO_AREA;TELEFONO" + '\r\n'
     lector_csv = csv.reader(contenido.splitlines())
-
+    
     for fila in lector_csv:
         linea = fila[0]  # Obtener el primer elemento de la fila como la línea a procesar
         resultado = estandarizador.estandarizar(linea)  # Obtener el resultado como una lista
         Dataframe1["cadenas"].append(linea)
         Dataframe1["tipo"].append(resultado[0])
         Dataframe1["indicativos_pais"].append(resultado[1])
+        Dataframe1["indicativos_area"].append(resultado[2])
+        Dataframe1["telefonos"].append(resultado[3])
 
-        if len(resultado) >= 4:
-            Dataframe1["indicativos_area"].append(resultado[2])
-            Dataframe1["telefonos"].append(resultado[3])
-        else:
-            Dataframe1["indicativos_area"].append(None)
-            Dataframe1["telefonos"].append(None)
-
+        
         resultado_str = linea + ";" + ";".join(str(item) for item in resultado)  # Convertir cada elemento en una cadena de texto
         datos += resultado_str + '\r\n'
 
     # Crear un DataFrame con los resultados
     df = pd.DataFrame(Dataframe1)
 
-    # Mostrar el resultado en una tabla en la parte principal
-    st.title("Resultado:")
-    st.dataframe(df)
+    # Mostrar el resultado en una tabla en la columna derecha
+    col2.title("Resultado:")
+    col2.dataframe(df)
 
-    # Crear botón para descargar archivo csv en la parte principal
-    st.download_button('Download CSV', datos, file_name=name + '.csv')
+    # Crear botón para descargar archivo csv en la columna derecha
+    col2.download_button('Download CSV', datos, file_name=name + '.csv')
+
+else:
+    st.warning("Por favor, selecciona un archivo para cargar.")
 
 else:
     st.warning("Por favor, selecciona un archivo para cargar.")
