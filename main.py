d787938e1a59bd5abe14e3929e9197f7543de037
Alejandra-byte-pixel <138229513+Alejandra-byte-pixel@users.noreply.github.com>
2023-07-14 01:46:34 -0500

    main.py

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ col1, col2 = st.columns(2)
 # Contenedor principal
 col1.markdown("<div class='main'>", unsafe_allow_html=True)
 
-
 # Mostrar el título en la parte superior
 col1.markdown("<h1 style='color: white;'>Aplicación para la Estandarización de teléfonos nacionales</h1>", unsafe_allow_html=True)
 
@@ -57,7 +56,6 @@ if nombre_archivo is not None:
         Dataframe1["indicativos_area"].append(resultado[2])
         Dataframe1["telefonos"].append(resultado[3])
 
-       
         resultado_str = linea + ";" + ";".join(str(item) for item in resultado)  # Convertir cada elemento en una cadena de texto
         datos += resultado_str + '\r\n'
 
@@ -65,19 +63,11 @@ if nombre_archivo is not None:
     df = pd.DataFrame(Dataframe1)
 
     # Mostrar el resultado en una tabla en la columna derecha
-    col2.markdown("""
-        <div style="position: sticky; top: 0;">
-            <h2>Resultado:</h2>
-        </div>
-    """, unsafe_allow_html=True)
+    col2.title("Resultado:")
     col2.dataframe(df)
 
     # Crear botón para descargar archivo csv en la columna derecha
-    col2.markdown("""
-        <div style="position: sticky; top: 50px;">
-            <a href="data:file/csv;base64,{b64}" download="{file_name}.csv">Download CSV</a>
-        </div>
-    """.format(b64=datos.encode().hex(), file_name=name), unsafe_allow_html=True)
+    col2.download_button('Download CSV', datos, file_name=name + '.csv')
 
 else:
     st.warning("Por favor, selecciona un archivo para cargar.")
