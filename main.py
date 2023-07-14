1b0ff15c0ef9aa7f3e2f81f83685a094a9d5615d
Alejandra-byte-pixel <138229513+Alejandra-byte-pixel@users.noreply.github.com>
2023-07-14 01:48:56 -0500

    main.py

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import estandarizador
 import csv
 import pandas as pd
 
-# Establecer el color de fondo para la parte derecha
+# Establecer el color de fondo para la barra lateral
 st.markdown(
     """
     <style>
-    .main {
-        background-color: #800000;
+    .stSidebar {
+        background-color: #8B0000;
         padding: 20px;
         color: white;
+        height: 100vh;
+        width: 300px;
     }
     </style>
     """,
@@ -18,13 +20,13 @@ st.markdown(
 )
 
 # Barra lateral (sidebar)
-
 st.sidebar.image("LogoAIO.jpeg", caption='ALL IN ONE', use_column_width=True)
-st.sidebar.markdown("<h1 style='text-align: center; color: white;'>Aqui cargue el Archivo</h1>", unsafe_allow_html=True)
+st.sidebar.markdown("<h1 style='text-align: center; color: red;'>Cargar Archivo</h1>", unsafe_allow_html=True)
 nombre_archivo = st.sidebar.file_uploader("Selecciona un archivo", type=["txt"])
 
+
 # Contenedor principal
-st.title("Aplicación para la Estandarización de teléfonos nacionales")
+st.title("Aplicación Estandarización de teléfonos nacionales")
 
 Dataframe1 = {
     "cadenas": [],
@@ -61,19 +63,12 @@ if nombre_archivo is not None:
     # Crear un DataFrame con los resultados
     df = pd.DataFrame(Dataframe1)
 
-    # Dividir la parte derecha en columnas
-    col1, col2 = st.beta_columns(2)
+    # Mostrar el resultado en una tabla
+    st.write("Resultado:")
+    st.dataframe(df)
 
-    # Mostrar la imagen y el título en la columna izquierda
-    col1.image("LogoAIO.jpeg", caption='ALL IN ONE', use_column_width=True)
-    col1.markdown("<h1 style='text-align: center; color: white;'>Cargar Archivo</h1>", unsafe_allow_html=True)
-
-    # Mostrar el resultado en una tabla en la columna derecha
-    col2.title("Resultado:")
-    col2.dataframe(df)
-
-    # Crear botón para descargar archivo csv en la columna derecha
-    col2.download_button('Download CSV', datos, file_name=name + '.csv')
+    # Crear botón para descargar archivo csv
+    st.download_button('Download CSV', datos, file_name=name + '.csv')
 
 else:
     st.warning("Por favor, selecciona un archivo para cargar.")
