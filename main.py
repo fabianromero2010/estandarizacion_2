b3c6474b6f01e8b1edfbc45c64e5a9734f02ebcb
Alejandra-byte-pixel <138229513+Alejandra-byte-pixel@users.noreply.github.com>
2023-07-14 01:16:20 -0500

    main.py

--- main.py
+++ main.py
@@ -52,7 +52,6 @@ if nombre_archivo is not None:
         Dataframe1["cadenas"].append(linea)
         Dataframe1["tipo"].append(resultado[0])
         Dataframe1["indicativos_pais"].append(resultado[1])
-
         if len(resultado) >= 4:
             Dataframe1["indicativos_area"].append(resultado[2])
             Dataframe1["telefonos"].append(resultado[3])
